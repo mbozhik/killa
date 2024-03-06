1e5837407638712eba5346ae8a1b486929696aca
max <bozzhik@ya.ru>
2024-03-06 05:11:04 +0300

    install `shadcn/ui` & add `accordion` component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,17 @@ import type {Config} from 'tailwindcss'
 
 const config: Config = {
   mode: 'jit',
+  darkMode: ['class'],
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontWeight: {
         book: '450',
@@ -17,6 +25,20 @@ const config: Config = {
           primary: '#275D50',
           f3: '#F3E6D6',
         },
+      },
+      keyframes: {
+        'accordion-down': {
+          from: {height: '0'},
+          to: {height: 'var(--radix-accordion-content-height)'},
+        },
+        'accordion-up': {
+          from: {height: 'var(--radix-accordion-content-height)'},
+          to: {height: '0'},
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
     screens: {
@@ -30,7 +52,8 @@ const config: Config = {
     plugin(function sizePlugin(api) {
       api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
     }),
+    [require('tailwindcss-animate')],
   ],
-}
+} satisfies Config
 
 export default config
