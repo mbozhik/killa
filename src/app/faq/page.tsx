b9edbe5301a1c731012706ba295eade4f24056a5
max <bozzhik@ya.ru>
2024-03-06 05:10:58 +0300

    init `faq` page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,3 +1,20 @@
 export default function FAQ() {
-  return <main>faq</main>
+  const faqContent = {
+    text: {
+      title: 'FAQ',
+      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+    },
+  }
+
+  return (
+    <main className="">
+      <section data-section="faq-main" className="grid w-screen h-screen place-items-center">
+        <div className="flex flex-col justify-center gap-5">
+          <h1 className="text-4xl font-black text-center text-custom-primary">{faqContent.text.title}</h1>
+
+          <div className="w-[60%] font-medium mx-auto text-xl text-center">{faqContent.text.description}</div>
+        </div>
+      </section>
+    </main>
+  )
 }
