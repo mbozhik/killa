d807f7ff3fc4dc81d6595064f2c31ec7cdd48165
max <bozzhik@ya.ru>
2024-03-06 15:53:00 +0300

    import `dropdown-menu` from `shadcn/ui` for `HeaderSocials`

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 import logoImage from '../../assets/logo.png'
+import HeaderSocials from './HeaderSocials'
 
 export default function Header() {
   return (
@@ -10,7 +10,8 @@ export default function Header() {
         <div className="w-36">
           <Image quality={100} src={logoImage} alt="" className="object-contain s-full" />
         </div>
-        <h1 className="text-xl font-semibold tracking-tight">Socials</h1>
+
+        <HeaderSocials />
       </div>
     </header>
   )
---
+++ src/components/ui/HeaderSocials.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from '@/components/ui/dropdown-menu'
+
+import xLogo from '../../assets/socials/x.svg'
+import instLogo from '../../assets/socials/inst.svg'
+import discordLogo from '../../assets/socials/discord.svg'
+
+export default function HeaderSocials() {
+  const socialsData = [
+    {title: 'Twitter', logo: xLogo, link: 'https://twitter.com/test'},
+    {title: 'Instagram', logo: instLogo, link: 'https://instagram.com/test'},
+    {title: 'Discord', logo: discordLogo, link: 'https://discord.gg/test'},
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="text-xl font-semibold tracking-tight border-none outline-none">SOCIALS</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {socialsData.map((social, index) => (
+          <Link key={index} target="_blank" href={social.link}>
+            <DropdownMenuItem className="flex gap-2">
+              <Image className="w-5" quality={100} src={social.logo} alt="" />
+              {social.title}
+            </DropdownMenuItem>
+          </Link>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
