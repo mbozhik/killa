fb14e7b88d80d116054ecc0a6e82c40710db3f91
max <bozzhik@ya.ru>
2024-03-09 14:03:13 +0300

    adaptive `Faq` block

--- src/components/index/Faq.tsx
+++ src/components/index/Faq.tsx
@@ -28,15 +28,15 @@ export default function Faq() {
   return (
     <section id="faq" data-section="faq-main" className="grid w-screen mt-[20vh] mb-[25vh] h-auto place-items-center">
       <div className="flex flex-col justify-center gap-5">
-        <h1 className="text-[125px] leading-none font-black text-center text-custom-primary">{faqContent.text.title}</h1>
+        <h1 className="font-black leading-none text-center sm:text-left sm:ml-5 text-9xl xl:text-7xl sm:text-4xl text-custom-primary">{faqContent.text.title}</h1>
 
-        <div className="text-2xl font-medium text-center" dangerouslySetInnerHTML={{__html: faqContent.text.description}}></div>
+        <div className="text-2xl font-medium text-center sm:text-left sm:text-lg sm:ml-5 xl:text-xl xl:tracking-normal" dangerouslySetInnerHTML={{__html: faqContent.text.description}}></div>
 
-        <Accordion type="multiple" className="flex flex-col gap-3 mt-5 w-[50vw]">
+        <Accordion type="multiple" className="flex flex-col gap-3 mt-5 w-[50vw] sm:w-full">
           {faqContent.accordionData.map((item, index) => (
             <AccordionItem key={index} value={item.id}>
-              <AccordionTrigger>{item.question}</AccordionTrigger>
-              <AccordionContent>{item.answer}</AccordionContent>
+              <AccordionTrigger className="sm:mx-5 sm:text-lg">{item.question}</AccordionTrigger>
+              <AccordionContent className="sm:mx-5 sm:text-lg">{item.answer}</AccordionContent>
             </AccordionItem>
           ))}
         </Accordion>
