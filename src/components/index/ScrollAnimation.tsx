bbb37a70a282253bba531e2a1fe03396ff44445d
bozzhik <bozzhik@ya.ru>
2024-03-19 16:47:09 +0300

    finalize `scroll` animation using `framer-motion`

--- src/components/index/ScrollAnimation.tsx
+++ src/components/index/ScrollAnimation.tsx
@@ -1,29 +1,45 @@
 'use client'
 
-import {motion, useScroll} from 'framer-motion'
 import Image from 'next/image'
+import {useState} from 'react'
+import {motion, useScroll, useMotionValueEvent} from 'framer-motion'
 
 import ScrollImage1 from '../../assets/scroll_animation/1.png'
 import ScrollImage2 from '../../assets/scroll_animation/2.png'
 import ScrollImage3 from '../../assets/scroll_animation/3.png'
 
-export default function ScrollAnimation() {
-  const aboutContent = {
-    imagesStyles: 'w-[70%] absolute inset-0 mx-auto',
-    images: [
-      {image: ScrollImage1, styles: 'absolute inset-0 mx-auto'},
-      {image: ScrollImage2, styles: 'absolute inset-0 mx-auto'},
-      {image: ScrollImage3, styles: 'absolute inset-0 mx-auto'},
-    ],
-  }
+const scrollContent = {
+  1: {src: ScrollImage1, wrapperStyles: 'justify-start', size: 'w-44'},
+  2: {src: ScrollImage2, wrapperStyles: 'justify-end', size: 'w-60'},
+  3: {src: ScrollImage3, wrapperStyles: 'justify-start', size: 'w-60', wrapperTransform: '-mt-52 ml-14'},
+}
 
-  const {scrollYProgress} = useScroll()
+export default function ScrollAnimation() {
+  const {scrollY} = useScroll()
+  const [hidden, setHidden] = useState(false)
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    if (latest > 1250) {
+      setHidden(false)
+    } else {
+      setHidden(true)
+    }
+  })
 
   return (
-    <section data-section="scroll-index" className="fixed inset-0 w-full -z-50">
-      {aboutContent.images.map((imageData, index) => (
-        <motion.div key={index} className={aboutContent.imagesStyles} style={{opacity: scrollYProgress}}>
-          <Image src={imageData.image} alt={`Image ${index + 1}`} />
+    <section data-section="scroll-index" className="fixed inset-0 w-[95%] mx-auto -z-50 grid grid-cols-2 place-content-center">
+      {Object.keys(scrollContent).map((index) => (
+        <motion.div
+          variants={{
+            visible: {y: 0, opacity: 100},
+            hidden: {y: '-100%', opacity: 0},
+          }}
+          animate={hidden ? 'hidden' : 'visible'}
+          transition={{duration: 0.75, ease: 'easeInOut'}}
+          className={`grid ${scrollContent[index].wrapperStyles} ${scrollContent[index].wrapperTransform}`}
+          key={index}
+        >
+          <Image className={`animate-swing-${index} ${scrollContent[index].size}`} src={scrollContent[index].src} alt={`image ${index + 1}`} />
         </motion.div>
       ))}
     </section>
