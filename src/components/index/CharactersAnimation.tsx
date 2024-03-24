b82a2a953595404c714e1d9186f0d29d9350e18b
bozzhik <bozzhik@ya.ru>
2024-03-25 00:03:05 +0300

    add multiple objects to `characters` animation

--- src/components/index/CharactersAnimation.tsx
+++ src/components/index/CharactersAnimation.tsx
@@ -1,62 +1,54 @@
 'use client'
 
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {motion} from 'framer-motion'
 
-import ScrollImage from '../../assets/scroll_animation/2.png'
+import ScrollImage1 from '../../assets/scroll_animation/1.png'
+import ScrollImage2 from '../../assets/scroll_animation/2.png'
+import ScrollImage3 from '../../assets/scroll_animation/3.png'
 
 const scrollContent = {
-  1: {src: ScrollImage, position: ''},
+  1: {src: ScrollImage1, position: {x: 0, y: 0}, speed: {x: 37.5, y: 37.5}},
+  2: {src: ScrollImage2, position: {x: 100, y: 200}, speed: {x: -50, y: -37.5}},
+  3: {src: ScrollImage3, position: {x: 300, y: 150}, speed: {x: 62.5, y: -50}},
 }
 
-const SPEED = 75
 const CIRCLE_SIZE = 100
 
 export default function App() {
-  const [x, setX] = useState(0)
-  const [speedX, setSpeedX] = useState(SPEED)
-  const [y, setY] = useState(0)
-  const [speedY, setSpeedY] = useState(SPEED)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setX(Math.min(x, document.documentElement.clientWidth - CIRCLE_SIZE))
-      setY(Math.min(y, document.documentElement.clientHeight - CIRCLE_SIZE))
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [x, y])
+  const [scrollImages, setScrollImages] = useState(scrollContent)
 
   return (
     <section className="fixed -z-50 inset-0 overflow-hidden">
-      {Object.keys(scrollContent).map((scrollNumber) => (
+      {Object.keys(scrollImages).map((index) => (
         <motion.img
-          key={scrollNumber}
-          src={scrollContent[scrollNumber].src.src}
-          alt={`image ${scrollNumber}`}
-          className={`s-40 absolute opacity-25`}
-          animate={{x: x, y: y}}
+          key={index}
+          className="s-40 absolute opacity-25"
+          alt={`image ${index}`}
+          src={scrollImages[index].src.src}
+          animate={{x: scrollImages[index].position.x, y: scrollImages[index].position.y}}
           transition={{ease: 'linear'}}
           onAnimationComplete={() => {
-            let nextX = x + speedX
-            let nextY = y + speedY
+            setScrollImages((prevState) => {
+              const newState = {...prevState}
+              let nextX = newState[index].position.x + newState[index].speed.x
+              let nextY = newState[index].position.y + newState[index].speed.y
 
-            if (nextX >= document.documentElement.clientWidth - CIRCLE_SIZE || nextX <= 0) {
-              nextX = Math.max(0, Math.min(nextX, document.documentElement.clientWidth - CIRCLE_SIZE))
-              setSpeedX(-speedX)
-            }
+              if (nextX >= document.documentElement.clientWidth - CIRCLE_SIZE || nextX <= 0) {
+                nextX = Math.max(0, Math.min(nextX, document.documentElement.clientWidth - CIRCLE_SIZE))
+                newState[index].speed.x = -newState[index].speed.x
+              }
 
-            if (nextY >= document.documentElement.clientHeight - CIRCLE_SIZE || nextY <= 0) {
-              nextY = Math.max(0, Math.min(nextY, document.documentElement.clientHeight - CIRCLE_SIZE))
-              setSpeedY(-speedY)
-            }
+              if (nextY >= document.documentElement.clientHeight - CIRCLE_SIZE || nextY <= 0) {
+                nextY = Math.max(0, Math.min(nextY, document.documentElement.clientHeight - CIRCLE_SIZE))
+                newState[index].speed.y = -newState[index].speed.y
+              }
 
-            setX(nextX)
-            setY(nextY)
+              newState[index].position.x = nextX
+              newState[index].position.y = nextY
+
+              return newState
+            })
           }}
         />
       ))}
