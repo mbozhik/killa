d83f9a2ddcf0c9967922a1835603f6127ead2364
paulgs9988 <paulgs9988@gmail.com>
2024-12-05 11:24:04 -0700

    updating config to point to tradeport-compatible package

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
-    PACKAGE_ID: "0xb7bda6b6a1c68a331cd797d6b78aaecebbad710a79321314321eca7797db99ad", // Current Testnet SC Package Object ID
-    COLLECTION_DATA_ID: "0x0dfff5286d06159c63231a40f1b2f560218ce1fb40b0d3cbd8bdb557d8bf74ca", // Corresponding CD Object ID
+    PACKAGE_ID: "0x5ab0845a183bbcf6c952e7723a5af8ee2efe58a4dbc43eb730a8e196dd68e54e", // Current Testnet SC Package Object ID
+    COLLECTION_DATA_ID: "0x03f82f3657c5e68422a4d6e81b95727b1bcadaabce9a455c1b82ad2dcf5db2ed", // Corresponding CD Object ID
     CLOCK_ID: "0x6",
     MINT_PRICE: BigInt(100_000_000), // 0.1 SUI
     MAX_MINT_PER_TX: 10
