c96672f56cc4591a16aa7f5ec68fa20758c95047
maxim <bozzhik@ya.ru>
2024-12-24 21:34:57 +0300

    edit content of `Faq` index block

--- src/components/index/Faq.tsx
+++ src/components/index/Faq.tsx
@@ -19,43 +19,45 @@ const parseAnswer = (answer: string) => {
 export default function Faq() {
   const faqContent = {
     title: 'FAQ',
-    description: 'What is the total supply of the collection? The Killa Club will be home to 2,222 retro, soulful bunnies running amok on SUI.',
     accordionData: [
       {
         id: '1',
-        question: 'When will the mint take place, and what will the mint price be?',
-        answer: 'These details are [REDACTED] for now. Follow our X account https://x.com/killaclubsui and join our discord to be notified when we announce this!',
+        question: 'What is the total supply of the collection?',
+        answer: 'The Killa Club will be home to 2,222 retro, soulful bunnies running amok on SUI.',
       },
       {
         id: '2',
-        question: 'How can I get on the whitelist?',
-        answer: "Visit our X account https://x.com/killaclubsui and follow the hints for access to the Hunter's Circle.",
+        question: 'When will the mint take place, and what will the mint price be?',
+        answer: 'The mint will go live on January 6, 2025. The mint price will be 14 SUI for the whitelist phase, and 18 SUI for the public phase. Follow our X account (https://x.com/killaclubsui) to stay up to date with all the details surrounding our upcoming launch!',
       },
       {
         id: '3',
-        question: 'What are the future plans for the project?',
-        answer: 'Beautiful art first - cool sh*t later. That is the mantra at the Killa Club - although Killa Club is an art project first, we plan on establishing an on-chain foundation through our community base. $BUNZ token for the Killa Club ecosystem, incubator style support for SUI projects, intellectual property (IP) using our brand kit and aesthetic, ecosystem airdrops, on-chain gaming dApps... the pipeline is limitless!',
+        question: 'How can I get on the whitelist?',
+        answer: "Join our Discord and visit our X account (https://x.com/killaclubsui) to follow the hints for access to the Hunter's Circle.",
       },
       {
         id: '4',
+        question: 'What are the future plans for the project?',
+        answer: `Beautiful art first - cool sh*t later. That is the mantra at the Killa Club - although Killa Club is an art project first, we plan on establishing an on-chain foundation through our community base. $BUNZ token for the Killa Club ecosystem, incubator style support for SUI projects, intellectual property (IP) using our brand kit and aesthetic, ecosystem airdrops, on-chain gaming dApps... the pipeline is limitless! For more information on the Killa Club's vision and direction for the collection, please read our Manifesto in the next FAQ.`,
+      },
+      {
+        id: '5',
         question: 'Where can I read the whitepaper?',
-        answer: 'Come check out the Killa Club Manifesto here: https://killa-club-manifesto.gitbook.io/killaclub',
+        answer: 'Come check out the Killa Club Manifesto here, where you can dive into our project: https://killa-club-manifesto.gitbook.io/killaclub',
       },
     ],
   }
 
   return (
-    <section id="faq" data-section="faq-index" className="grid mt-[15vh] mb-[25vh] w-[55%] sm:w-[95%] mx-auto h-auto place-items-center">
+    <section id="faq" data-section="faq-index" className="grid mt-[15vh] mb-[25vh] sm:mb-[15vh] w-[55%] sm:w-[97%] mx-auto h-auto place-items-center">
       <div className="flex flex-col justify-center w-full gap-5">
-        <h1 className="font-black leading-none text-center sm:text-left sm:ml-5 text-9xl xl:text-7xl sm:text-4xl text-custom-primary">{faqContent.title}</h1>
-
-        <div className="text-2xl text-center sm:text-left sm:ml-5 xl:text-xl sm:text-base xl:tracking-normal mx-auto max-w-[45ch]">{faqContent.description}</div>
+        <h1 className="font-black leading-none text-center text-9xl xl:text-7xl sm:text-4xl text-custom-primary">{faqContent.title}</h1>
 
         <Accordion type="multiple" className="flex flex-col w-full gap-3.5 mx-auto mt-5">
           {faqContent.accordionData.map((item, index) => (
             <AccordionItem value={item.id} key={index}>
-              <AccordionTrigger className="sm:mx-5 sm:text-base">{item.question}</AccordionTrigger>
-              <AccordionContent className="sm:mx-5 sm:text-base max-w-[80ch]">{parseAnswer(item.answer)}</AccordionContent>
+              <AccordionTrigger className="sm:mx-3 sm:text-lg sm:text-left sm:!leading-[1.35]">{item.question}</AccordionTrigger>
+              <AccordionContent className="sm:mx-3 sm:text-base sm:text-left sm:!leading-[1.3] max-w-[80ch]">{parseAnswer(item.answer)}</AccordionContent>
             </AccordionItem>
           ))}
         </Accordion>
--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -13,7 +13,7 @@ AccordionItem.displayName = 'AccordionItem'
 
 const AccordionTrigger = React.forwardRef<React.ElementRef<typeof AccordionPrimitive.Trigger>, React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>>(({className, children, ...props}, ref) => (
   <AccordionPrimitive.Header className="flex">
-    <AccordionPrimitive.Trigger ref={ref} className={cn('flex flex-1 items-center justify-between bg-custom-f3/90 ring-2 ring-custom-primary text-2xl xl:text-xl text-custom-primary rounded-md p-4 font-medium transition-all hover:text-opacity-80 [&[data-state=open]>svg]:rotate-180', className)} {...props}>
+    <AccordionPrimitive.Trigger ref={ref} className={cn('flex flex-1 sm:gap-2 items-center justify-between bg-custom-f3/90 ring-2 ring-custom-primary text-2xl xl:text-xl text-custom-primary rounded-md p-4 sm:px-5 font-medium transition-all hover:text-opacity-80 [&[data-state=open]>svg]:rotate-180', className)} {...props}>
       {children}
       <ChevronDown className="transition-transform duration-300 s-7 shrink-0" />
     </AccordionPrimitive.Trigger>
