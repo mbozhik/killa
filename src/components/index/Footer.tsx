0e8586c2c47fe508971e1c64e927af5a6a5dbc5a
max <bozzhik@ya.ru>
2025-01-06 10:57:05 +0300

    edit `Footer` index component

--- src/components/index/Footer.tsx
+++ src/components/index/Footer.tsx
@@ -1,9 +1,19 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <footer className="w-[55%] sm:w-[97%] mx-auto h-auto">
       <div className="flex justify-between mt-3 mb-4 text-lg tracking-normal uppercase sm:my-3 sm:items-center sm:gap-0 sm:flex-col xl:text-sm text-custom-primary">
-        <span>©️ 2024 Killa Club Inc.</span>
-        <span>All Rights Reserved.</span>
+        <div className="flex gap-1.5">
+          <span>
+            ©️ 2024 <em className="underline not-italic underline-offset-[1.5px]">Killa Club Inc</em>.
+          </span>
+          <span>All Rights Reserved.</span>
+        </div>
+
+        <Link className="hover:underline hover:underline-offset-[1.5px]" href="mailto:killaclubinc@gmail.com" target="_blank">
+          <span>killaclubinc@gmail.com</span>
+        </Link>
       </div>
     </footer>
   )
