032fc208e68e31931b96b6d739d1912de3896253
Wesleyss071299 <wesleysilvadesousa@gmail.com>
2025-01-19 18:01:11 -0300

    fix: live

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -485,7 +485,7 @@ export default function MintPage() {
                     Eligible for whitelisted phases
                   </button>
                   <button
-                    onClick={handleVip}
+                    onClick={() => {}}
                     className={cn(buttonStyles, "disabled:opacity-50 mt-2", "")}
                   >
                     Mint
